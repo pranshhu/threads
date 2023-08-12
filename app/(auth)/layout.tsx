7e8c28fbaaa1592e21ff277092a640fd5b376a3a
Pranshu Gupta <pranshu11111@gmail.com>
2023-08-12 19:01:12 +0530

    v0.1

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs"
+
+export const metadata = {
+    title: 'Threads',
+    description: 'A Next.js 13 Meta Threads Application'
+}
+
+export  default function RootLayout({ 
+    children 
+}: { 
+    children: React.ReactNode 
+}) {
+    return (
+    <ClerkProvider>
+        <html lang= "en">
+        <body>
+            
+        </body>
+        </html>
+    </ClerkProvider>
+    )
+}
