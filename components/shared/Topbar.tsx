c49cc2ff567335ce867dc62af7bf04eb47d8a142
Pranshu Gupta <pranshu11111@gmail.com>
2023-08-13 16:52:45 +0530

    v2

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
 function Topbar(){
     return (
     <nav className="topbar">
-        <Link ></Link>
+        <Link href="/" className="flex item-center gap-4">
+            <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+            <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+        </Link>
     </nav>
     )
 }
