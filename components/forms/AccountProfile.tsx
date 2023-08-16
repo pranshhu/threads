a44c56cb1e44855bf36498b8d4fdd00b48402395
Pranshu Gupta <pranshu11111@gmail.com>
2023-08-16 23:25:39 +0530

    .

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,7 +1,22 @@
 "use client"
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import { UserValidation } from '@/lib/validations/user';
+import * as z from "zod";
 
 interface Props{
-    user{
+    user:{
         id: string;
         objectId: string;
         username: string;
@@ -11,11 +26,45 @@ interface Props{
     btnTitle: string;
 }
 
-const AccountProfile = ({ user , btnTitle } : Props) => {
+const AccountProfile = ({ user, btnTitle } : Props) => {
+    const form = useForm({
+        resolver: zodResolver(UserValidation), 
+        defaultValues: {
+            profile_photo: '',
+            name: '',
+            user: '',
+            bio: ''
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof UserValidation>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        console.log(values)
+      }
+
     return (
-        <div>
-            Account Profile
-        </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
     )
 }
 
